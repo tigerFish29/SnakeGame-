c292d62e22ccc2df677c5c3a6b94ed3872a36599
tigerFish29 <paulwchabz28@gmail.com>
2022-06-23 01:08:31 +0100

    First commit for the snake game in java

---
+++ GamePanel.java
@@ -0,0 +1,57 @@
+package com.qa.paul.snake;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel  extends JPanel implements ActionListener{
+    GamePanel() { // constructor
+
+    }
+
+    public void startGame()  {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+    // draw method
+    public void draw(Graphics g) {
+
+    }
+    // move function
+    public void move() {
+
+    }
+    // check apple
+    public void checkApple() {
+
+    }
+
+    // check for collisions
+    public void checkCollisions() {
+
+    }
+
+    // game over method
+    public void gameOver(Graphics g) {
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e ) {
+        // auto
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+}
+
---
+++ SnakeGame.java
@@ -0,0 +1,9 @@
+package com.qa.paul.snake;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();
+
+    }
+}
